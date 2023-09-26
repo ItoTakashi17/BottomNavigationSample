8c513278f2e1ea9019381630780515eb2384494b
yp888 <yp18227206844@gmail.com>
2023-09-26 08:41:10 +0800

    v1.2 权限请求请参考FilesFragment和清单文件 动画无法启用

--- app/src/main/java/com/yipeng/bottomnavigationsample/ui/files/FilesFragment.kt
+++ app/src/main/java/com/yipeng/bottomnavigationsample/ui/files/FilesFragment.kt
@@ -1,16 +1,21 @@
 package com.yipeng.bottomnavigationsample.ui.files
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaScannerConnection
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.yipeng.bottomnavigationsample.databinding.FragmentFilesBinding
 import java.io.File
 import java.io.FileOutputStream
@@ -23,33 +28,44 @@ class FilesFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        val filesViewModel =
-            ViewModelProvider(this).get(FilesViewModel::class.java)
-
         _binding = FragmentFilesBinding.inflate(inflater, container, false)
         val editText = binding.editTextTextMultiLine
         val buttonForSave = binding.buttonForSave
         buttonForSave.setOnClickListener {
             val newText = editText.text.toString()
-            val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            val downloadsDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
             val folderPath = File(downloadsDir, "AirCast")
             if (!folderPath.exists()) {
                 folderPath.mkdirs()
             }
-            if (ContextCompat.checkSelfPermission(requireContext(),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)  {
-                val file = File(folderPath, "hello.txt")
-                if (file.createNewFile()) {
-                    FileOutputStream(file).use {
-                        it.write(newText.toByteArray())
+            when{
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(context, "已授权", Toast.LENGTH_SHORT).show()
+                    val file = File(folderPath, "$newText.mp3")
+                    if (file.createNewFile()) {
+                        FileOutputStream(file).use {
+                            it.write(newText.toByteArray())
+                        }
+                        MediaScannerConnection.scanFile(context, arrayOf(file.path), null, null)
                     }
-                    MediaScannerConnection.scanFile(context, arrayOf(file.path), null, null)
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.MANAGE_EXTERNAL_STORAGE) -> {
+                    Toast.makeText(context, "我们需要您授予储存权限才能保存文件到设备", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(context, "我们需要您授予储存权限才能保存文件到设备", Toast.LENGTH_SHORT).show()
+                    requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 222)
                 }
             }
         }
@@ -64,4 +80,19 @@ class FilesFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        if (requestCode == REQUEST_CODE) {
+//            // 检查权限是否已经获取
+//            if (ContextCompat.checkSelfPermission(
+//                    requireContext(),
+//                    Manifest.permission.MANAGE_EXTERNAL_STORAGE
+//                ) == PackageManager.PERMISSION_GRANTED
+//            ) {
+//
+//            }
+//
+//        }
+//
+//    }
 }
